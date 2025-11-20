578259d9e9442165e09839e20325c248435b0e28
AkshatContemy <akshat@contemy.io>
2025-11-20 14:35:52 +0530

    End scene updated

--- Export.js
+++ Export.js
@@ -2,7 +2,7 @@ const puppeteer = require("puppeteer");
 const { PuppeteerScreenRecorder } = require("puppeteer-screen-recorder");
 const path = require("path");
 
-const filename = "bidmyroom_search.html";
+const filename = "scene-end.html";
 const outputFilename = filename.replace(".html", ".mp4");
 
 (async () => {
@@ -11,7 +11,8 @@ const outputFilename = filename.replace(".html", ".mp4");
   await page.goto(`file://${path.join(__dirname, filename)}`, {
     waitUntil: "networkidle0",
   });
-  await page.setViewport({ width: 2000, height: 2000 });
+  // await page.setViewport({ width: 2000, height: 2000 });
+  await page.setViewport({ width: 1920, height: 1080 });
 
   // // Wait for the Macbook skin image to load
   // await page.evaluate(() => {
@@ -29,14 +30,14 @@ const outputFilename = filename.replace(".html", ".mp4");
   const recorder = new PuppeteerScreenRecorder(page, {
     followNewTab: true,
     fps: 60,
-    videoCrf: 23, // Quality (lower = better)
+    videoCrf: 20, // Quality (lower = better)
     videoPreset: "ultrafast",
     videoCodec: "libx264",
   });
   await recorder.start(outputFilename);
 
   // Let animation run
-  await new Promise((resolve) => setTimeout(resolve, 22000));
+  await new Promise((resolve) => setTimeout(resolve, 12000));
 
   await recorder.stop();
   await browser.close();
